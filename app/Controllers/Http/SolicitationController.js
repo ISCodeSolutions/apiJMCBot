6f3fcbb99f848bcd94fff5359eddcfa0436a1e23
Edvaldo Silva <edvaldodeveloper@gmail.com>
2021-02-01 11:35:41 -0300

    Ajuste Body-Parser

--- app/Controllers/Http/SolicitationController.js
+++ app/Controllers/Http/SolicitationController.js
@@ -14,6 +14,7 @@ class SolicitationController {
    * @param {View} ctx.view
    */
     index({request, response, view}){
+        //Comentario
 
         let intentName = request.body.queryResult.intent.displayName;
 
